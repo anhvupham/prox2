1e0d8961af4fcf113fa8ef42c766eb0bbf77b79d
Vincent Pham <anhvu.phamduong@gmail.com>
2015-12-24 21:04:45 -0800

    optimization

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -1,22 +1,7 @@
-var pid = [],
-    fs = require('fs'),
+var fs = require('fs'),
     path = require('path'),
     ansi = require('ansi-html'),
     mkdirp = require('mkdirp');
-
-function serviceIsRunning(id) {
-    var i = 0;
-    for (i; i < pid.length; i += 1) {
-        if (pid[i].model.id === id) {
-            if (pid[i].dead) {
-                return false
-            } else {
-                return true
-            }
-        }
-    }
-    return false;
-};
 
 function updateProcessPID(id, pid, log) {
     Index.findOne({
@@ -39,24 +24,17 @@ function updateProcessPID(id, pid, log) {
                         logs: log
                     });
                 } else {
-                    // console.log('shoudl be append')
-
                     process.logs = process.logs.map(function(item) {
                         if (item.pid == pid) {
                             item.logs += log;
-                            // item.logs += 'aaaaaaaaaaaaaaaaa';
-                            // console.log('log.logs', item.logs);
                         }
                         return item;
                     });
                 }
             }
-            // console.log(JSON.stringify(process.logs));
             process.save(function(err, doc) {
                 if (err) {
                     console.log('Error when update process info : ', err);
-                } else {
-                    // console.log(require('util').format('Update PID for process %s succeeded', doc.name));
                 }
             });
         }
@@ -96,7 +74,6 @@ module.exports = {
             });
         }
     },
-
     findOne: function(req, res) {
         Index.findOne({
             id: req.params.id
@@ -112,7 +89,6 @@ module.exports = {
             }
         });
     },
-
     POST: function(req, res) {
         var service = req.body;
         // console.log(service);
@@ -143,8 +119,6 @@ module.exports = {
             }
         });
     },
-
-    // Updates A Service Entry
     PUT: function(req, res) {
         Index.destroy({
             id: req.params.id
@@ -180,7 +154,6 @@ module.exports = {
             }
         });
     },
-    // DELETES A Service From The Service List
     DELETE: function(req, res) {
         Index.destroy({
             id: req.params.id
@@ -223,79 +196,72 @@ module.exports = {
             }
         });
     },
-    // Starts A Service By It's ID (HTTP POST) path : '/execute/:id',
     StartService: function(req, res) {
-        if (!serviceIsRunning(req.params.id)) {
-            var service = req.body.service;
-            if (service) {
-                start();
-            } else {
-                Index.find({
-                    id: req.params.id
-                }, function(err, process) {
-                    if (err) {
-                        onError(err, res);
-                    } else {
-                        service = process[0];
-                        if (service) {
-                            var custom = false;
-                            for (var i = 0; i < service.args.length; i++) {
-                                if (service.args[i].match(/\?/g)) {
-                                    custom = true;
-                                }
-                            }
-                            if (!custom) {
-                                start();
-                            } else {
-                                //ask client to build the service with custom command
-                                res.status(200).json({
-                                    needbuild: true,
-                                    service: service
-                                });
-                            }
-                        } else {
-                            onError('Service not found', res);
-                        }
-                    }
-                });
-            }
-
-            function start() {
-                var log, pid;
-                var svc = Proc.start(service, function(stdout) {
-                    stdout = stdout.replace(/\n$/, '').replace(/\n/g, '\n' + service.name + ' >  ');
-                    console.log(service.name + ' >  ' + stdout);
-
-                    sails.sockets.blast("log", {
-                        id: service.id,
-                        log: (ansi(stdout) + "<br/>").replace(/\n/g, '<br/>')
-                    });
-                    log = stdout;
-                    // updateProcessPID(req.params.id, pid, log);
-                });
-                pid = svc.pid;
-                // Update PID to database
-                updateProcessPID(req.params.id, svc.pid);
-
-                svc.on('close', function(code) {
-                    console.log('\n--------------------------------------------------------'.red)
-                    console.log((service.name + " has died with code " + code).red);
-                    console.log('--------------------------------------------------------\n'.red)
-
-                    // Update PID to database
-                    updateProcessPID(req.params.id, '');
-
-                });
-                res.status(200).json({
-                    service: service
-                });
-            }
+        var service = req.body.service;
+        if (service) {
+            start();
         } else {
-            console.log("  > Nimble: Refusing To Start ".red + (req.params.id).red + " Because it is already running".red)
+            Index.find({
+                id: req.params.id
+            }, function(err, process) {
+                if (err) {
+                    onError(err, res);
+                } else {
+                    service = process[0];
+                    if (service) {
+                        var custom = false;
+                        for (var i = 0; i < service.args.length; i++) {
+                            if (service.args[i].match(/\?/g)) {
+                                custom = true;
+                            }
+                        }
+                        if (!custom) {
+                            start();
+                        } else {
+                            //ask client to build the service with custom command
+                            res.status(200).json({
+                                needbuild: true,
+                                service: service
+                            });
+                        }
+                    } else {
+                        onError('Service not found', res);
+                    }
+                }
+            });
+        }
+
+        function start() {
+            var log, pid;
+            var svc = Proc.start(service, function(stdout) {
+                stdout = stdout.replace(/\n$/, '').replace(/\n/g, '\n' + service.name + ' >  ');
+                console.log(service.name + ' >  ' + stdout);
+
+                sails.sockets.blast("log", {
+                    id: service.id,
+                    log: (ansi(stdout) + "<br/>").replace(/\n/g, '<br/>')
+                });
+                log = stdout;
+                // updateProcessPID(req.params.id, pid, log);
+            });
+            pid = svc.pid;
+            // Update PID to database
+            updateProcessPID(req.params.id, svc.pid);
+
+            svc.on('close', function(code) {
+                console.log('\n--------------------------------------------------------'.red)
+                console.log((service.name + " has died with code " + code).red);
+                console.log('--------------------------------------------------------\n'.red)
+
+                // Update PID to database
+                updateProcessPID(req.params.id, '');
+
+            });
+            res.status(200).json({
+                service: service
+            });
         }
     },
-
-    // Stops A Service By It's ID (HTTP DELETE) path : '/execute/:id',
     StopService: function(req, res) {
         Index.findOne({
             id: req.params.id
